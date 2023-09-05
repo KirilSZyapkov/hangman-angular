b5910ad4a901550e86853c921738fe000e70de5a
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-05 23:59:08 +0300

    Drowing the body.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { words } from 'word';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'hangman';
-  word: string[] = words[Math.floor(Math.random()*words.length)].split("");
+  // word: string[] = words[Math.floor(Math.random() * words.length)].split('');
+  word: string[] = ['t', 'e','s','t'];
   guessedLetters: string[] = ['t', 's', 'g'];
-
+  incorrectLetters: string[] = this.guessedLetters.filter(
+    (letter) => !this.word.includes(letter)
+  );
 }
--- src/app/components/hangman-drawing/hangman-drawing.component.ts
+++ src/app/components/hangman-drawing/hangman-drawing.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hangman-drawing',
   templateUrl: './hangman-drawing.component.html',
-  styleUrls: ['./hangman-drawing.component.css']
+  styleUrls: ['./hangman-drawing.component.css'],
 })
 export class HangmanDrawingComponent {
+  @Input() incorrectLetters: number = 0;
+
+   body_parts: string[] = [
+    "head",
+    "body",
+    "left-arm",
+    "right-arm",
+    "left-leg",
+    "right-leg"
+  ];
+
+  show() {
+    console.log(this.incorrectLetters);
+  }
 
 }
