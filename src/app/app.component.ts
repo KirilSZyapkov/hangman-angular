e9d3e451a3356b04a6ea138c4363da4bf211f871
Kiril Zyapkov <ziapkov@gmail.com>
2023-09-06 23:34:48 +0300

    Implementing keybord event.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { words } from 'word';
 
 @Component({
@@ -7,23 +7,30 @@ import { words } from 'word';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
+  @HostListener('window:keypress', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(event.key);
+    this.guessedLetters.push(event.key);
+    this.incorrectLetter();
+  }
   title = 'hangman';
   word: string[] = words[Math.floor(Math.random() * words.length)].split('');
-  
+
   guessedLetters: string[] = [];
   incorrectLetters: string[] = [];
-  
-  guessedLetter(letter: string): void{
+
+  guessedLetter(letter: string): void {
     console.log(letter);
     this.guessedLetters.push(letter);
     this.incorrectLetter();
   }
 
-  incorrectLetter(): void{
+  incorrectLetter(): void {
     this.incorrectLetters = this.guessedLetters.filter(
       (letter) => !this.word.includes(letter)
     );
   }
-
+  show(e: Event) {
+    console.log(e);
+  }
 }
